8d6ff981a73c3af160f64524b1548cf8f2d73c24
dolezfil <dolezfil@fel.cvut.cz>
2024-03-13 19:28:29 +0100

    Added game loop and layered draw method

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let canvas = document.querySelector(".gameCanvas");
 let ctx = canvas.getContext('2d');
-let img = new Image();
+let background = new Image();
+let player = new Image();
 
 function createCanvas(width, height){
     // Set canvas width and height
@@ -10,14 +11,44 @@ function createCanvas(width, height){
 
 function setup(){
 
-    img.src = './images/background.png';
     let width = 1000;
     let height = 1000;
     createCanvas(width, height);
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, width, height);
-    };
-    
+    background.src = './images/background.png';
+    player.src = './images/player_test.png';
+}
+
+function draw(){
+    ctx.drawImage(background, 0, 0, 1000, 1000);
+    ctx.drawImage(player, 0, 0, 100, 100);
+}
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+function animate(){
+    // request another frame
+    requestAnimationFrame(animate);
+
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+        then = now - (elapsed % fpsInterval);
+
+        draw();
+    }
 }
 
 setup();
+
+window.onload = function(){
+    startAnimating(90);
+};
