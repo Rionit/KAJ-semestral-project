3601d4d7cfac131c2e623f9a0b58d3b6ab99ef6d
dolezfil <dolezfil@fel.cvut.cz>
2024-03-13 21:08:18 +0100

    Added sprites object and foreach sprite in draw

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+import { Sprite } from './modules/sprites.js'
+
 let canvas = document.querySelector(".gameCanvas");
 let ctx = canvas.getContext('2d');
-let background = new Image();
-let player = new Image();
+const sprites = {
+    background: new Sprite('./images/background.png'),
+    player: new Sprite('./images/player_test.png', true, 2)
+};
+
+let animFrame = 0;
+let animationSpeed = 200;
 
 function createCanvas(width, height){
     // Set canvas width and height
@@ -10,18 +17,25 @@ function createCanvas(width, height){
 }
 
 function setup(){
-
     let width = 1000;
     let height = 1000;
     createCanvas(width, height);
-    background.src = './images/background.png';
-    player.src = './images/player_test.png';
 }
 
 function draw(){
-    ctx.drawImage(background, 0, 0, 1000, 1000);
-    ctx.drawImage(player, 0, 0, 100, 100);
+    for (const key in sprites) {
+        const sprite = sprites[key];
+        const { x, y } = sprite;
+
+        if (sprite.animated) {
+            ctx.drawImage(sprite.image, sprite.getFrameX(animFrame), 0, sprite.frameWidth, sprite.image.height, x, y, x + sprite.frameWidth, y + sprite.image.height);
+        } else {
+            ctx.drawImage(sprite.image, x, y, x + sprite.image.width, y + sprite.image.height);
+        }
+    }
 }
+
+var fps, fpsInterval, startTime, now, then, elapsed;
 
 function startAnimating(fps) {
     fpsInterval = 1000 / fps;
@@ -48,6 +62,10 @@ function animate(){
 }
 
 setup();
+
+const intervalId = setInterval(() => {
+    animFrame++;
+}, animationSpeed);
 
 window.onload = function(){
     startAnimating(90);
