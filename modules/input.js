625cd93225c37ead212c15b87be7dedfb55fe6fe
dolezfil <dolezfil@fel.cvut.cz>
2024-04-27 14:46:54 +0200

    Added arcade buttons

--- modules/input.js
+++ modules/input.js
@@ -16,9 +16,41 @@ export class Input{
         const key = e.key;
         if (key in this.#pressedKeys) {
             this.#pressedKeys[key] = (e.type === 'keydown');
+
+            // Get the corresponding circle based on the pressed key
+            const circleId = this.getCircleId(key);
+            const circle = document.querySelector(circleId);
+
+            if (circle) {
+                // Add or remove the animation class based on key press
+                if (this.#pressedKeys[key]) {
+                    // circle.style.strokeWidth = 15;
+                    circle.style.transform = 'translateY(7px)';
+                    circle.style.filter = 'drop-shadow(0px 0px 0px rgb(0 0 0 / 1))';
+                } else {
+                    // circle.style.strokeWidth = 5;
+                    circle.style.transform = 'translateY(-7px)';
+                    circle.style.filter = 'drop-shadow(0px 14px 0px rgb(0 0 0 / 1))';
+                }
+            }
         } else {
             console.log("Error: (" + e.key + ") key was not found!");
         }
+    }
+
+    getCircleId(key) {
+        // Map keys to circle IDs
+        const keyToCircleId = {
+            w: '.upBtn',
+            a: '.leftBtn',
+            s: '.downBtn',
+            d: '.rightBtn',
+            ArrowDown: 'circle3',
+            ArrowUp: 'circle1',
+            ArrowLeft: 'circle2',
+            ArrowRight: 'circle4'
+        };
+        return keyToCircleId[key];
     }
 
     get playerDirection() {
