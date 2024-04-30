37820d0d4ec12f456763f79be7ac20549533df52
dolezfil <dolezfil@fel.cvut.cz>
2024-04-30 16:50:15 +0200

    Changed styles and colors

--- modules/leaderboard.js
+++ modules/leaderboard.js
@@ -3,7 +3,14 @@ export class Leaderboard {
     leaderboardData = [
         { name: "Player 1", score: 100 },
         { name: "Player 2", score: 90 },
-        { name: "Player 3", score: 80 }
+        { name: "Player 3", score: 80 },
+        { name: "Player 4", score: 90 },
+        { name: "Player 5", score: 10 },
+        { name: "Player 6", score: 12 },
+        { name: "Player 7", score: 1 },
+        { name: "Player 8", score: 5 },
+        { name: "Player 9", score: 250 },
+        { name: "Player 10", score: 40 }
     ];
     #table
 
@@ -16,7 +23,7 @@ export class Leaderboard {
     // Function to render leaderboard
     renderLeaderboard() {
         this.#table.innerHTML = "";
-
+        this.leaderboardData.sort((a, b) => b.score - a.score);
         this.leaderboardData.forEach((player, index) => {
             const row = document.createElement("tr");
             row.innerHTML = `
