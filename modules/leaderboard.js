20a7090893ce56fc61c81ff266746b98d5060837
dolezfil <dolezfil@fel.cvut.cz>
2024-05-11 14:42:22 +0200

    Improved leaderboard

--- modules/leaderboard.js
+++ modules/leaderboard.js
@@ -1,15 +1,22 @@
 export class Leaderboard {
 
     leaderboardData = [
-        { name: "Player 1", score: 100 },
-        { name: "Player 2", score: 90 },
-        { name: "Player 3", score: 80 },
-        { name: "Player 4", score: 90 },
+        { name: "Zdenek Vlach", score: 250 },
+        { name: "Ondrej Zara", score: 100 },
+        { name: "Petr Hurtak", score: 90 },
+        { name: "Jan Dusek", score: 80 },
+        { name: "Player 1", score: 70 },
         { name: "Player 5", score: 10 },
         { name: "Player 6", score: 12 },
         { name: "Player 7", score: 1 },
         { name: "Player 8", score: 5 },
-        { name: "Player 9", score: 250 },
+        { name: "Player 11", score: 1 },
+        { name: "Player 12", score: 2 },
+        { name: "Player 13", score: 3 },
+        { name: "Player 14", score: 4 },
+        { name: "Player 15", score: 5 },
+        { name: "Player 16", score: 6 },
+        { name: "Player 17", score: 0 },
         { name: "Player 10", score: 40 }
     ];
     #table
@@ -20,20 +27,35 @@ export class Leaderboard {
         this.renderLeaderboard();
     }
 
-    // Function to render leaderboard
-    renderLeaderboard() {
-        this.#table.innerHTML = "";
-        this.leaderboardData.sort((a, b) => b.score - a.score);
-        this.leaderboardData.forEach((player, index) => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td></td>
-                <td>${player.name}</td>
-                <td>${player.score}</td>
-            `;
-            this.#table.appendChild(row);
-        });
+    addRow(player, index=null) {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${index ?? ''}${index ? '.' : ''}</td>
+            <td>${player.name}</td>
+            <td>${player.score}</td>
+        `;
+        this.#table.appendChild(row);
     }
+
+    renderLeaderboard() {
+        const len = this.leaderboardData.length;
+        this.#table.innerHTML = "";
+        // Sort leaderboard data
+        this.leaderboardData.sort((a, b) => b.score - a.score);
+        // Slice to get the top 15 players
+        const topPlayers = this.leaderboardData.slice(0, 15);
+        // Add top 15 players to the leaderboard
+        topPlayers.forEach((player) => {
+            this.addRow(player);
+        });
+    
+        // Show last player
+        if (len > 15) {
+            const lastPlacePlayer = this.leaderboardData[len - 1];
+            this.addRow(lastPlacePlayer, len - 1);
+        }
+    }
+    
 
     // Function to add a new player to the leaderboard
     addPlayer(name, score) {
